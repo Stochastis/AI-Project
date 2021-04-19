da06a5fdcd7b4c0e1cba0ae1306dc0cb1827877f
Stochastis <calebsross@gmail.com>
2021-04-19 11:04:18 -0500

    Now calculating dot product with numpy.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import numpy as np
+
 inputVector = [1.72, 1.23]
 weights1 = [1.26, 0]
 weights2 = [2.17, 0.32]
 
 # Computing the dot product of inputVector and weights1
-firstIndexesMult = inputVector[0] * weights1[0]
-secondIndexesMult = inputVector[1] * weights1[1]
-dotProduct1 = firstIndexesMult + secondIndexesMult
+dotProduct1 = np.dot(inputVector, weights1)
 
 print(f"The dot product is: {dotProduct1}")
