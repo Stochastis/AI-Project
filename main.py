09ada7ea93dbbcf749df3f67e189cffac26b5e70
Stochastis <calebsross@gmail.com>
2021-04-19 11:06:25 -0500

    Now calculating both dot products.

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ weights2 = [2.17, 0.32]
 
 # Computing the dot product of inputVector and weights1
 dotProduct1 = np.dot(inputVector, weights1)
+dotProduct2 = np.dot(inputVector, weights2)
 
-print(f"The dot product is: {dotProduct1}")
+print(f"The first dot product is: {dotProduct1}")
+print(f"The second dot product is: {dotProduct2}")
