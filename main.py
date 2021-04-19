faf30e1b57b0bada376fe63ce03bd731cb2c1ce0
Stochastis <calebsross@gmail.com>
2021-04-19 11:42:52 -0500

    Calculating Mean Squared Error(MSE).

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Wrapping the vectors in NumPy arrays
 inputVector = np.array([2, 1.5])
-weights1 = np.array([1.45, -0.66])
+weights = np.array([1.45, -0.66])
 bias = np.array([0.0])
 
 
@@ -10,12 +10,13 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def make_prediction(input_vector, weights, bias_param):
-    layer1 = np.dot(input_vector, weights) + bias_param
+def make_prediction(input_vector, weights_param, bias_param):
+    layer1 = np.dot(input_vector, weights_param) + bias_param
     layer2 = sigmoid(layer1)
     return layer2
 
 
-prediction = make_prediction(inputVector, weights1, bias)
-
-print(f"The prediction result is: {prediction}")
+prediction = make_prediction(inputVector, weights, bias)
+target = 0
+mse = np.square(prediction - target)
+print(f"Prediction: {prediction}; error: {mse}")
