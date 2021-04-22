cd58b07843e4fc03b6c9b481f330c4f69db008b3
Stochastis <calebsross@gmail.com>
2021-04-22 13:22:53 -0500

    Test cases.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -81,6 +82,13 @@ class NeuralNetwork:
         return cumulative_errors
 
 
+input_vectors = np.array([[3, 1.5], [2, 1], [4, 1.5], [3, 4], [3.5, 0.5], [2, 0.5], [5.5, 1], [1, 1]])
+targets = np.array([0, 1, 0, 1, 0, 1, 1, 0])
 learning_rate = 0.1
 neural_network = NeuralNetwork(learning_rate)
-print(f'DEBUG: prediction = {neural_network.predict([1, 2])}')
+training_error = neural_network.train(input_vectors, targets, 10000)
+
+plt.plot(training_error)
+plt.xlabel("Iterations")
+plt.ylabel("Error for all training instances")
+plt.savefig("cumulative_error.png")
